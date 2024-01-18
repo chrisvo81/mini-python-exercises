20e3a751d7e7ef5dbdb93cad6474ac58c640f227
Chris V <vochris81@gmail.com>
2024-01-17 21:05:00 -0500

    add the game higher-lower-search

---
+++ higher-lower-search/main.py
@@ -0,0 +1,50 @@
+from art import logo, vs
+from game_data import data
+from random import randint
+from replit import clear
+
+STARTING_SCORE = 0
+BEGINING_GAME = False
+
+def pick_random_account():
+  return data[randint(0, len(data) - 1)]
+
+def compare_followers(followers_a, followers_b):
+  if followers_a > followers_b:
+    return 'A'
+  else:
+    return 'B'
+
+def print_versus(account_a, account_b):
+  print(f"Compare A: {account_a['name']}, a {account_a['description']}, from {account_a['country']}.")
+  print(vs)
+  print(f"Against B: {account_b['name']}, a {account_b['description']}, from {account_b['country']}.")
+  
+
+def main():
+  print(logo)
+  score = STARTING_SCORE
+  game_over = BEGINING_GAME
+  account_a = pick_random_account()
+  account_b = pick_random_account()
+
+  while account_b == account_a:
+    account_b = pick_random_account()
+  
+  while not game_over:
+    print_versus(account_a, account_b)
+    guess = input("Who has more followers? Type 'A' or 'B': ")
+    winner = compare_followers(int(account_a['follower_count']), int(account_b['follower_count']))
+    
+    if guess == winner:
+      score += 1
+      account_a = account_b
+      account_b = pick_random_account()
+      clear()
+      print(f"You're right! Current score: {score}.")
+    else:
+      game_over = True
+      clear()
+      print(f"Sorry, that's wrong. Final score: {score}")
+
+main()

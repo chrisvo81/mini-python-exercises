29d581cf57af1a6165b57257c80fc9c3328c67b5
Chris Vo <chrisvo@ChrisV.local>
2024-04-14 20:10:47 -0400

    Add turtle-gui practice

---
+++ turtle-gui/README.md
@@ -0,0 +1,6 @@
+# Turtle GUI
+
+## Document
+Turtle Graphics: https://docs.python.org/3/library/turtle.html
+
+Tk color specification: https://www.tcl.tk/man/tcl8.4/TkCmd/colors.htm
---
+++ turtle-gui/main.py
@@ -0,0 +1,43 @@
+from turtle import Turtle, Screen
+
+def create_turtle(input_colour='red'):
+    turtle = Turtle()
+    turtle.shape('turtle')
+    turtle.color(input_colour)
+    return turtle
+
+
+def forward_dash_line(turtle, num_dashes=15, dash_length=10):
+    for _ in range(num_dashes):
+        turtle.forward(dash_length)
+        turtle.pu()
+        turtle.forward(dash_length)
+        turtle.pd()
+
+
+def draw_square(turtle, turn_direction='right'):
+    for _ in range(4):
+        if turn_direction == 'right':
+            turtle.right(90)
+        else:
+            turtle.left(90)
+        turtle.forward(100)
+
+
+def main():
+    # Use a breakpoint in the code line below to debug your script.
+    timmy = create_turtle('green')
+    forward_dash_line(timmy)
+
+    # tommy = create_turtle()
+    # draw_square(tommy, 'left')
+
+    screen = Screen()
+    screen.exitonclick()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/

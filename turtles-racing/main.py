a1547438ddac3f1a176dd45de99a92d058b14b11
Chris Vo <chrisvo@MAXBOK15.local>
2024-06-03 22:29:10 -0400

    Refacotring turtle racing

--- turtles-racing/main.py
+++ turtles-racing/main.py
@@ -1,7 +1,9 @@
 from turtle import Turtle, Screen
+import random
 
 
 rainbow_colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+all_turtles = []
 
 
 def validate_input():
@@ -22,13 +24,43 @@ def turtle_setup(turtle, index):
     turtle.goto(x=-230, y=position)
 
 
+def initialize_turtles():
+    for i in range(len(rainbow_colors)):
+        new_turtle = Turtle(shape='turtle')
+        new_turtle.color(rainbow_colors[i])
+        turtle_setup(new_turtle, i)
+        all_turtles.append(new_turtle)
+
+
+def announce_result(winner, bet):
+    if winner == bet:
+        print(f"You've won! The {winner} turtle is the winner.")
+    else:
+        print(f"You've lost! The {winner} turtle is the winner.")
+
+
+def start_race():
+    winning_turtle = None
+
+    while not winning_turtle:
+        for turtle in all_turtles:
+            rand_distance = random.randint(0, 10)
+            turtle.forward(rand_distance)
+
+            if turtle.xcor() > 230:
+                winning_turtle = turtle.pencolor()
+                break
+
+    return winning_turtle
+
+
 def main():
     user_bet = validate_input()
-    print(user_bet)
-    for i in range(len(rainbow_colors)):
-        turt = Turtle(shape='turtle')
-        turt.color(rainbow_colors[i])
-        turtle_setup(turt, i)
+
+    if user_bet:
+        initialize_turtles()
+        winning_turtle = start_race()
+        announce_result(winner=winning_turtle, bet=user_bet)
 
 
 # Press the green button in the gutter to run the script.
@@ -38,4 +70,3 @@ if __name__ == '__main__':
     main()
     screen.exitonclick()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/

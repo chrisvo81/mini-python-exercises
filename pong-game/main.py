b982fbb8eee77a52b2d080c80604b3c91843c5dc
chrisvo81 <vochris81@gmail.com>
2024-07-25 22:46:37 -0400

    when paddles miss the ball logic

--- pong-game/main.py
+++ pong-game/main.py
@@ -26,13 +26,21 @@ while game_is_on:
     screen.update()
     ball.move()
 
-    # Detect collision with wall
+    # Detect collision with top and bottom wall
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
     # Detect collision with r_paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+
+    # Detect when R paddle missed the ball
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # Detect when L paddle missed the ball
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 
 screen.exitonclick()

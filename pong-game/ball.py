cd98c7a249015d4e2ed79f0d14a53b58e205e073
chrisvo81 <vochris81@gmail.com>
2024-07-25 22:33:13 -0400

    bounce the ball

--- pong-game/ball.py
+++ pong-game/ball.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
 
+
 class Ball(Turtle):
 
     def __init__(self):
@@ -7,8 +8,13 @@ class Ball(Turtle):
         self.color("white")
         self.shape("square")
         self.penup()
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+
+    def bounce(self):
+        self.y_move *= -1
